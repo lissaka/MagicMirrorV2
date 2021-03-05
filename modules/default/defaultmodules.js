45bf8b450d21685ddd07fe39219b51b5577a5101
lissaka <64579054+lissaka@users.noreply.github.com>
2021-03-05 19:27:33 +0100

    Update defaultmodules.js

--- modules/default/defaultmodules.js
+++ modules/default/defaultmodules.js
@@ -7,7 +7,7 @@
 
 // Modules listed below can be loaded without the 'default/' prefix. Omitting the default folder name.
 
-var defaultModules = ["compliments","MMM-CoinMarketCap", "clock", "currentweather", "newsfeed", "MMM-RATP", "MMM-Trello"];
+var defaultModules = ["MMM-Ratp","compliments","MMM-CoinMarketCap", "clock", "currentweather", "newsfeed", "MMM-RATP", "MMM-Trello"];
 //var defaultModules = ["MMM-horoscope","MMM-CyberSecurityNews","MMM-RATP"];
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
